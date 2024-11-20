6d01373a9b15c813d79b8e8bcbb122b8f87ef0a2
e-ian <ianemma70@gmail.com>
2024-11-20 14:12:46 +0300

    - add transaction request model

---
+++ src/Interswitch.Api/Models/TransactionRequest.cs
@@ -0,0 +1,10 @@
+// src/Interswitch.Api/Models/TransactionRequest.cs
+namespace Interswitch.Api.Models;
+
+public class TransactionRequest
+{
+    public string MerchantId { get; set; } = "";
+    public decimal Amount { get; set; }
+    public string Currency { get; set; } = "NGN";
+    public string Reference { get; set; } = "";
+}
